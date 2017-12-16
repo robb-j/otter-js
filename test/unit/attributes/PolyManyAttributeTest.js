0f6b8c153e6421ddae1a289d6556281e60121f56
Rob Anderson <robster31@googlemail.com>
2017-12-16 21:48:06 +0000

    Working on PolyMany

--- test/unit/attributes/PolyManyAttributeTest.js
+++ test/unit/attributes/PolyManyAttributeTest.js
@@ -11,7 +11,7 @@ describe('PolyManyAttribute', function() {
   let Entity, CompA, CompB, TestOtter
   beforeEach(async function() {
     Entity = makeModel('Entity', {
-      comp: { type: 'PolyMany', clusters: [ 'CompA', 'CompB' ] }
+      comps: { type: 'PolyMany', clusters: [ 'CompA', 'CompB' ] }
     })
     CompA = makeCluster('CompA', { name: String })
     CompB = makeCluster('CompB', { size: Number })
@@ -46,7 +46,7 @@ describe('PolyManyAttribute', function() {
   describe('#processOptions', function() {
     it('should store types', async function() {
       await TestOtter.start()
-      let Types = Entity.schema.comp.PolyTypes
+      let Types = Entity.schema.comps.PolyTypes
       expect(Types).to.exist
       expect(Types.CompA).to.equal(CompA)
     })
@@ -54,29 +54,75 @@ describe('PolyManyAttribute', function() {
   
   describe('#installOn', function() {
     
-    // ...
+    
+    let entity
+    beforeEach(async function() {
+      await TestOtter.start()
+      entity = new Entity({ comps: [
+        { _type: 'CompA', name: 'tasty' },
+        { _type: 'CompB', size: 100 }
+      ]})
+    })
+    
+    it('should set an initial value', async function() {
+      expect(entity.comps).to.exist
+    })
+    
+    describe('~getter', function() {
+      it('should return values', async function() {
+        expect(entity.comps).to.have.lengthOf(2)
+      })
+    })
+    
+    describe('~setter', function() {
+      it('should set using cluster instances', async function() {
+        let toSet = [
+          new CompA({ name: 'fresh' }),
+          new CompB({ size: 120 })
+        ]
+        entity.comps = toSet
+        expect(entity.comps).to.deep.equal(toSet)
+      })
+      it('should set using objects with a _type', async function() {
+        entity.comps = [
+          { _type: 'CompB', size: 12 },
+          { _type: 'CompB', size: 31 },
+          { _type: 'CompA', name: 'yummy' }
+        ]
+        expect(entity.comps).to.have.lengthOf(3)
+        expect(entity.comps[0]).to.be.instanceof(CompB)
+        expect(entity.comps[1]).to.be.instanceof(CompB)
+        expect(entity.comps[2]).to.be.instanceof(CompA)
+      })
+    })
   })
   
-  // describe('#validateModelValue', function() {
-  //   beforeEach(async function() {
-  //     await TestOtter.start()
-  //   })
-  //   it('should validate using super', async function() {
-  //     let value = 7
-  //     let error = await asyncError(() => Entity.schema.comp.validateModelValue(value))
-  //     expect(error).to.have.property('code', 'attr.validation.type')
-  //   })
-  //   it('should fail for invalid types', async function() {
-  //     let value = { _type: 'Invalid' }
-  //     let error = await asyncError(() => Entity.schema.comp.validateModelValue(value))
-  //     expect(error.code).to.equal('attr.poly.invalidType')
-  //   })
-  //   it('should validate using the type', async function() {
-  //     let value = { _type: 'CompA', name: 7 }
-  //     let error = await asyncError(() => Entity.schema.comp.validateModelValue(value))
-  //     expect(error).to.have.property('code', 'composite')
-  //     expect(error.subCodes).to.include('attr.validation.type')
-  //   })
-  // })
+  describe('#validateModelValue', function() {
+    beforeEach(async function() {
+      await TestOtter.start()
+    })
+    it('should validate using super', async function() {
+      let value = 7
+      let error = await asyncError(() => Entity.schema.comps.validateModelValue(value))
+      expect(error).to.have.property('code', 'attr.validation.type')
+    })
+    it('should fail for non-arrays', async function() {
+      let value = { test: 5 }
+      let error = await asyncError(() => Entity.schema.comps.validateModelValue(value))
+      expect(error).to.have.property('code', 'attr.validation.notArray')
+    })
+    it('should fail for invalid types', async function() {
+      let value = [ { _type: 'Invalid' }, { noType: true } ]
+      let error = await asyncError(() => Entity.schema.comps.validateModelValue(value))
+      expect(error).to.have.property('code', 'composite')
+      expect(error.subCodes).to.include('attr.poly.invalidType')
+    })
+    it('should validate using the type', async function() {
+      let value = [ { _type: 'CompA', name: 100 } ]
+      let error = await asyncError(() => Entity.schema.comps.validateModelValue(value))
+      expect(error).to.have.property('code', 'composite')
+      expect(error.subCodes).to.include('attr.validation.type')
+    })
+  })
   
 })
